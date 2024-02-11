e7364d22eff7155d7ac85e34be8fc85d2bcb5e8d
Pramod <giripramodsunway789@gmail.com>
2024-02-11 23:21:41 +0545

    Second update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FaBars, FaTimes} from 'react-icons/fa'
 
 const Navbar = () => {
+
+  const [nav, setNav] = useState(false);
+
+  const links = [
+    {
+      id: 1,
+      link: 'home'
+    },
+    {
+      id: 2,
+      link: 'about'
+    },
+    {
+      id: 3,
+      link: 'projects'
+    },
+    {
+      id: 4,
+      link: 'experiences'
+    },
+    {
+      id: 5,
+      link: 'contact'
+    },
+  ]
   return (
     <div className="flex justify-between items-center w-full h-20 text-white
     bg-black fixed px-4">
         <div>
-            <h1 className="text-5xl font-signature ml-2">Pramod</h1>
+            <h1 className="text-5xl font-signature ml-2">Portfolio</h1>
         </div>
 
-        <ul className="flex">
-            <li className="px-4 cursor-pointer font-medium text-gray-500 hover:scale-105 capitalize duration-200">home</li>
-            <li className="">Home</li>
+        <ul className="hidden md:flex">
+          {links.map(({id, link}) => (
+            <li key={id} className="px-4 cursor-pointer font-medium text-gray-500 hover:scale-105 capitalize duration-200">{link}</li>
+            
+
+          ))}
 
 
+          
+
+        </ul>
+
+        <div onClick={() => setNav(!nav)} className="cursor-pointer pr-4 z-50 text-gray-500">
+          {nav ? <FaTimes size={30} />: <FaBars size={30} />}
+        </div>
+
+        <ul className="flex flex-col justify-center" >
+          <li>Home</li>
         </ul>
     </div>
 
