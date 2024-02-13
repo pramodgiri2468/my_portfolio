42d21bc6d19bf8d1bc8529a84461002aea7968e2
Pramod <giripramodsunway789@gmail.com>
2024-02-13 22:55:45 +0545

    Third time update

---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import HeroImage from '../assets/my_portfolio/hero1.jpg';
+const Home = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Home;
