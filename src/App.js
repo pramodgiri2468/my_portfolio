72196b6f652163601f950b3ecb20c2db727fa995
Pramod <giripramodsunway789@gmail.com>
2024-02-21 11:20:39 +0545

    Fourth updated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
+import About from "./components/About";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
 import Sociallinks from "./components/Sociallinks";
 
 function App() {
@@ -7,6 +9,8 @@ function App() {
     <div>
       <Navbar />
       <Home />
+      <About />
+      <Projects />
 
       
       <Sociallinks />
---
+++ src/components/About.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div name="about" className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white">
+    
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full  ">
+        <div className="pb-8 ">
+            <p className="text-4xl font-bold inline border-b-4 border-gray-500 ">About</p>
+        </div>
+        <p className="text-xl mt-20 ">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Voluptate aliquam eveniet adipisci voluptas praesentium repellat libero excepturi, 
+            beatae fugit eius velit a voluptates molestias pariatur facilis cupiditate. 
+            Odit porro quidem laboriosam adipisci modi tempore pariatur. 
+            Quia voluptate illum modi voluptatibus. 
+            Commodi assumenda est veritatis aut error quam sapiente facere minima?
+        </p>
+        <br />
+
+    
+
+        <p className="text-xl ">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Voluptatem nulla culpa atque earum quo quos ex impedit vel. 
+            Nemo ratione sequi in minus rerum aperiam fugit quasi minima esse deserunt magni, 
+            officiis accusamus nobis non? Illo debitis tempora ex consequuntur perferendis provident minima illum, 
+            alias numquam velit deserunt expedita vitae adipisci aperiam voluptas? Autem culpa rem, 
+            modi libero natus porro.
+        </p>
+
+        
+      </div>
+    </div>
+  )
+};
+
+export default About;
